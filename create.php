d0ddcd8d262e44901a36ca222be9483156c92173
mayar <aseel3sal@gmail.com>
2021-01-11 15:56:38 +0100

    added create.php

---
+++ create.php
@@ -0,0 +1,37 @@
+<?php
+
+// declare(strict_types=1);
+require __DIR__ . '/app/autoload.php';
+require __DIR__ . '/views/header.php';
+// require __DIR__ . '/app/users/avatar.php';
+
+
+?>
+<article>
+    <h1>Sign up</h1>
+
+    <form action="app/users/create.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input class="form-control" type="email" name="email" id="email" placeholder="Your email address" required>
+            <small class="form-text text-muted">Please provide your email address*</small>
+        </div>
+        <!-- /form-group -->
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" name="password" id="password" required>
+            <small class="form-text text-muted">Please write your password (passphrase)*</small>
+        </div>
+        <!-- /form-group -->
+        <div class="form-group">
+            <label for="confirmPassword"> Confirm Password</label>
+            <input class="form-control" type="password" name="confirmPassword" id="confirmPassword" required>
+            <small class="form-text text-muted">Please re-write your password (passphrase)*</small>
+        </div>
+        <!-- /form-group -->
+
+        <button type="submit" class="btn btn-primary">Sign up</button>
+    </form>
+</article>
+
+<?php require __DIR__ . '/views/footer.php'; ?>
